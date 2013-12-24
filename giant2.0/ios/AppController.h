ee764f64dc9825e366cea9dd3f7dd34798efb8f7
AS <as@liuyangdeiMac.local>
2013-12-24 11:33:31 +0800

    Initial Commit

---
+++ giant2.0/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  giant2_0AppController.h
+//  giant2.0
+//
+//  Created by AS on 13-12-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ giant2.0/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  giant2_0AppController.h
+//  giant2.0
+//
+//  Created by AS on 13-12-24.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
